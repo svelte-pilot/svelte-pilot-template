fc45648b12f4f0e12fad107de3b8589dbebcd6d8
fenix.sol <jiangfengming87@gmail.com>
2023-10-12 02:16:35 +0800

    set 404 & 500 status code

--- src/views/404.svelte
+++ src/views/404.svelte
@@ -1,1 +1,10 @@
+<script lang="ts" context="module">
+  import type { Route } from 'svelte-pilot'
+  import type { Context } from '../context-interface'
+
+  export async function load(props: unknown, route: Route, ctx: Context) {
+    ctx.setStatus(404)
+  }
+</script>
+
 <h1>404 Not Found</h1>
--- src/views/500.svelte
+++ src/views/500.svelte
@@ -1,1 +1,10 @@
+<script lang="ts" context="module">
+  import type { Route } from 'svelte-pilot'
+  import type { Context } from '../context-interface'
+
+  export async function load(props: unknown, route: Route, ctx: Context) {
+    ctx.setStatus(500)
+  }
+</script>
+
 <h1>500 Error</h1>
