1e9b34cb9373807a6b40705949fc8e12c6786c0d
Jiang Fengming <jiangfengming87@gmail.com>
2023-11-18 23:03:45 +0800

    fix(node-adapter): Resolve homepage server-side rendering issue

--- src/adapters/node/index.ts
+++ src/adapters/node/index.ts
@@ -8,11 +8,17 @@ import template from '/dist/client/index.html?raw'
 const PORT = Number(process.env.PORT) || 5173
 const serve = sirv(join(fileURLToPath(import.meta.url), '../../client'))
 
-createServer(async (req, res) => {
+createServer((req, res) => {
   const url = req.url || '/'
   console.log(url)
 
-  serve(req, res, async () => {
+  if (url === '/') {
+    handler()
+  } else {
+    serve(req, res, handler)
+  }
+
+  async function handler() {
     const {
       statusCode = 200,
       statusMessage,
@@ -26,7 +32,7 @@ createServer(async (req, res) => {
 
     res.writeHead(statusCode, statusMessage, headers)
     res.end(body)
-  })
+  }
 }).listen(PORT)
 
 console.log(`Server running at http://localhost:${PORT}`)
